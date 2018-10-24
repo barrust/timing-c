0c0becaac5edd78052ac1610fd4e1e20fd8bd0ba
Tyler Barrus <barrust@gmail.com>
2018-10-23 20:39:13 -0400

    Minor changes (#1)
    * add aditional properties
    * add quick getters for struct objects
    * update README; tabs->spaces

--- timing.h
+++ timing.h
@@ -3,7 +3,7 @@
 ***	 Author: Tyler Barrus
 ***	 email:  barrust@gmail.com
 ***
-***	 Version: 1.0.3
+***	 Version: 1.0.4
 ***	 Purpose: Provide a simple format to gather timing information
 ***
 ***	 License: MIT 2015
@@ -21,32 +21,43 @@
 ***		 free(pretty_output);
 ***
 *******************************************************************************/
-#ifndef __TIMING_H__
-#define __TIMING_H__
+#ifndef BARRUST_TIMING_H__
+#define BARRUST_TIMING_H__
 
 
 #include <stdlib.h>
 #include <stdio.h>
 #ifdef _WIN32
-	#include <Windows.h>
-	#include <stdint.h>    // portable: uint64_t   MSVC: __int64
+    #include <Windows.h>
+    #include <stdint.h>    // portable: uint64_t   MSVC: __int64
 #else
-	#include <sys/time.h>  /* *nix based timing; doesn't support windows */
+    #include <sys/time.h>  /* *nix based timing; doesn't support windows */
 #endif
 
 
-#define TIMING_VERSION "1.0.3"
+#define TIMING_VERSION "1.0.4"
 #define TIMING_MAJOR 1
 #define TIMING_MINOR 0
-#define TIMING_REVISION 3
+#define TIMING_REVISION 4
 
 #define timing_get_version()        (TIMING_VERSION)
 #define timing_get_difference(t)    (t.timing_double)
+#define timing_get_hours(t)         (t.hours)
+#define timing_get_minutes(t)       (t.minutes)
+#define timing_get_seconds(t)       (t.seconds)
+#define timing_get_milliseconds(t)  (t.milliseconds)
+#define timing_get_microseconds(t)  (t.microseconds)
+
 
 typedef struct timing {
-	struct timeval end_time;
-	struct timeval start_time;
-	double timing_double;
+    struct timeval end_time;
+    struct timeval start_time;
+    double timing_double;
+    int hours;
+    short minutes;
+    short seconds;
+    short milliseconds;
+    short microseconds;
 } Timing;
 
 /* Begin timing section */
@@ -63,8 +74,8 @@ void calc_difference(Timing *t); /* only necessary, very occasionally */
 
 #ifdef _WIN32
 struct timeval {
-	long tv_sec;
-	long tv_usec;
+    long tv_sec;
+    long tv_usec;
 };
 int gettimeofday(struct timeval * tp, struct timezone * tzp);
 #endif
@@ -77,49 +88,48 @@ static long long timeval_diff(struct timeval *difference, struct timeval *end_ti
 *** PUBLIC FUNCTION DEFINITIONS
 *******************************************************************************/
 void timing_start(Timing *t) {
-	gettimeofday(&t->start_time, NULL);
-	t->end_time.tv_sec = 0;
-	t->end_time.tv_usec = 0;
-	t->timing_double = 0.0;
+    gettimeofday(&t->start_time, NULL);
+    t->end_time.tv_sec = 0;
+    t->end_time.tv_usec = 0;
+    t->timing_double = 0.0;
 }
 
 void timing_end(Timing *t) {
-	gettimeofday(&t->end_time, NULL);
-	calc_difference(t);
+    gettimeofday(&t->end_time, NULL);
+    calc_difference(t);
 }
 
 void calc_difference(Timing *t){
-	struct timeval difference;
-	timeval_diff(&difference, &t->end_time, &t->start_time);
-	t->timing_double = difference.tv_sec + (difference.tv_usec / 1000000.0);
+    struct timeval difference;
+    timeval_diff(&difference, &t->end_time, &t->start_time);
+    t->timing_double = difference.tv_sec + (difference.tv_usec / 1000000.0);
+
+    int s = difference.tv_sec;
+    int ms = difference.tv_usec;
+    t->hours = s / 3600;
+    t->minutes = (s % 3600) / 60;
+    t->seconds = (s % 3600) % 60;
+    t->milliseconds = ms / 1000;
+    t->microseconds = ms % 1000;
 }
 
 /*
-	format_time_diff allocates memory and returns a pointer to that memory location
-	NOTE: It is up to the caller to release this memory!
+    format_time_diff allocates memory and returns a pointer to that memory location
+    NOTE: It is up to the caller to release this memory!
 
-	E.g.,
-		char *result = format_time_diff(&t);
-		free(result);
+    E.g.,
+    char *result = format_time_diff(&t);
+    free(result);
 */
 char* format_time_diff(Timing *t) {
-	struct timeval difference;
-	// int digits = 14; // this is everything but the hours
-	timeval_diff(&difference, &t->end_time, &t->start_time);
-	int s = difference.tv_sec;
-	int ms = difference.tv_usec;
+    struct timeval difference;
+    timeval_diff(&difference, &t->end_time, &t->start_time);
 
-	// calculate the different pieces...
-	int hours = s / 3600;
-	int minutes = (s % 3600) / 60;
-	int seconds = (s % 3600) % 60;
-	int milliseconds = ms / 1000;
-	int microseconds = ms % 1000;
-	// put it into a string!
-	int len = snprintf(NULL, 0, "%02d:%02d:%02d:%03d.%03d", hours, minutes, seconds, milliseconds, microseconds);
-	char* res = calloc(len + 1, sizeof(char));
-	snprintf(res, len + 1, "%02d:%02d:%02d:%03d.%03d", hours, minutes, seconds, milliseconds, microseconds);
-	return res;
+    // put it into a string!
+    int len = snprintf(NULL, 0, "%02d:%02d:%02d:%03d.%03d", t->hours, t->minutes, t->seconds, t->milliseconds, t->microseconds);
+    char* res = calloc(len + 1, sizeof(char));
+    snprintf(res, len + 1, "%02d:%02d:%02d:%03d.%03d", t->hours, t->minutes, t->seconds, t->milliseconds, t->microseconds);
+    return res;
 }
 
 
@@ -127,21 +137,21 @@ char* format_time_diff(Timing *t) {
 *** PRIVATE FUNCTIONS
 *******************************************************************************/
 static long long timeval_diff(struct timeval *difference, struct timeval *end_time, struct timeval *start_time) {
-	struct timeval temp_diff;
-	// if not passed in, we will use this to do the calculation and
-	// then return the number of microseconds
-	if(difference == NULL) {
-		difference = &temp_diff;
-	}
-	difference->tv_sec = end_time->tv_sec - start_time->tv_sec;
-	difference->tv_usec = end_time->tv_usec - start_time->tv_usec;
+    struct timeval temp_diff;
+    // if not passed in, we will use this to do the calculation and
+    // then return the number of microseconds
+    if(difference == NULL) {
+    difference = &temp_diff;
+    }
+    difference->tv_sec = end_time->tv_sec - start_time->tv_sec;
+    difference->tv_usec = end_time->tv_usec - start_time->tv_usec;
 
-	/* Using while instead of if below makes the code slightly more robust. */
-	while(difference->tv_usec < 0) {
-		difference->tv_usec += 1000000;
-		difference->tv_sec -= 1;
-	}
-	return 1000000LL * difference->tv_sec + difference->tv_usec;
+    /* Using while instead of if below makes the code slightly more robust. */
+    while(difference->tv_usec < 0) {
+    difference->tv_usec += 1000000;
+    difference->tv_sec -= 1;
+    }
+    return 1000000LL * difference->tv_sec + difference->tv_usec;
 }
 
 
@@ -151,26 +161,26 @@ static long long timeval_diff(struct timeval *difference, struct timeval *end_ti
 *******************************************************************************/
 #ifdef _WIN32
 /*
-	NOTE: this ignores the timezone information since we don't need it
+    NOTE: this ignores the timezone information since we don't need it
 */
 int gettimeofday(struct timeval *tp, struct timezone *tzp) {
-	// Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-	static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
 
-	SYSTEMTIME  system_time;
-	FILETIME	file_time;
-	uint64_t	time;
+    SYSTEMTIME  system_time;
+    FILETIME	file_time;
+    uint64_t	time;
 
-	GetSystemTime(&system_time);
-	SystemTimeToFileTime(&system_time, &file_time);
-	time =  ((uint64_t)file_time.dwLowDateTime);
-	time += ((uint64_t)file_time.dwHighDateTime) << 32;
+    GetSystemTime(&system_time);
+    SystemTimeToFileTime(&system_time, &file_time);
+    time =  ((uint64_t)file_time.dwLowDateTime);
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
 
-	tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
-	tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
-	return 0;
+    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+    return 0;
 }
 #endif // end defining windows gettimeofday function
 
 
-#endif /* END OF TIMING.H */
+#endif /* END OF BARRUST_TIMING_H__ */
--- timing_test.c
+++ timing_test.c
@@ -33,4 +33,20 @@ int main(int argc, char **argv) {
     printf("time elapsed: %f\n", timing_get_difference(t));
     printf("difference as long long: %lld\t%s\n", i, tmp);
     free(tmp);
+
+     timing_start(&t);
+     // code to time here
+     for (unsigned long long i = 0; i < 50000000000; i++) {
+         // pass
+     }
+     timing_end(&t);
+     printf("code completed in %f seconds\n", t.timing_double);
+     char *pretty_output = format_time_diff(&t);
+     printf("code completed in %s (HH:MM:SS.MS.MLS)\n", pretty_output);
+     printf("hours:        %d\n", timing_get_hours(t));
+     printf("minutes:      %d\n", timing_get_minutes(t));
+     printf("seconds:      %d\n", timing_get_seconds(t));
+     printf("milliseconds: %d\n", timing_get_milliseconds(t));
+     printf("microseconds: %d\n", timing_get_microseconds(t));
+     free(pretty_output);
 }
